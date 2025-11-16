80ef02b07cca2898b1e40907135609355179d77a
abap34 <abap0002@gmail.com>
2025-11-16 18:40:52 +0900

    update example

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -18,17 +18,17 @@
 
         # Basic sanity checks
         @test !isempty(prog.functions)
-        @test haskey(prog.functions, :main) "$(filename) should have a main function"
+        @test haskey(prog.functions, :main)
 
         # Try to run the program
         # Some programs may require specific heap sizes or fail intentionally
         try
             result, state = run_program(prog; heap_size=100)
-            @test result isa LFVal "$(filename) should return a valid value"
+            @test result isa LFVal
 
             # Check that heap size is reasonable
-            @test state.m >= 0 "$(filename) should not have negative heap space"
-            @test length(state.σ) <= 100 "$(filename) should not exceed heap allocation"
+            @test state.m >= 0
+            @test length(state.σ) <= 100
         catch e
             # If the program fails, it might be intentional (e.g., testing heap exhaustion)
             # or a bug. For now, we'll rethrow to catch bugs
